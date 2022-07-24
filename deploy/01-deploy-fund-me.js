2a83a912ea28ae7674dd022743e71a92689fb2c9
thefishingdock <jeffreymurphysimon@gmail.com>
2022-07-24 13:30:43 +0700

    initial commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,43 @@
+// function deployFunc() {
+//     console.log("Hi!")
+// }
+
+const { getNamedAccounts } = require("hardhat")
+
+// module.exports.default = deployFunc
+
+// module.exports = async (hre) => {
+//     const { getNamedAccounts, deployments } = hre
+// }
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getnamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+    log("------------------------------------------------------")
+    // const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress],
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, [ethUsdPriceFeedAddress])
+    }
+}
+module.exports.tags = ["all", "fundme"]
